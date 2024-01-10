246942484f178e56bf8b59b8cc2dca696ef440d3
eliasyoung <elias.y.warlock@gmail.com>
2024-01-10 17:04:11 +0800

    202401101704

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,5 +1,7 @@
 use super::http::{Request, RequestMethod, Response, StatusCode};
 use super::server::Handler;
+use std::fs;
+
 pub struct WebsiteHandler {
     public_path: String,
 }
@@ -8,13 +10,28 @@ impl WebsiteHandler {
     pub fn new(public_path: String) -> WebsiteHandler {
         WebsiteHandler { public_path }
     }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("Path not in public directory: {}\n", file_path);
+                    None
+                }
+            }
+            Err(_) => None,
+        }
+    }
 }
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         match request.method() {
             RequestMethod::GET => match request.path() {
-                "/" => Response::new(StatusCode::Ok, Some("<h1>Hello World</h1>".to_string())),
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
                 "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello Page</h1>".to_string())),
                 _ => Response::new(StatusCode::NotFound, None),
             },
